e2e33b4f426fd62ce50b4a0425d8fa67a4b958b2
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 21:41:30 -0700

    updated_workout_code

--- string.js
+++ string.js
@@ -1,3 +1,24 @@
 for (let i = 0; i < str.length; i++) {
   console.log(str[i]);
 }
+
+
+
+// for (: This is the beginning of a for loop, which is a control structure used to repeat a certain block of code a specified number of times.
+
+//   let i = 0;: Here, you're initializing a variable i with the value 0.
+//  This variable will be used to keep track of the current index of the string as you iterate through it.
+  
+//   i < str.length;: This is the condition that determines when the loop should continue running. 
+// The loop will continue as long as the value of i is less than the length of the string str. 
+// The str.length gives the total number of characters in the string.
+  
+//   i++: After each iteration of the loop, this statement increments the value of i by 
+// 1. This means that in each iteration, you'll move to the next character in the string.
+  
+//   ) {: The closing parenthesis marks the end of the loop initialization and condition, and the opening curly brace { indicates the beginning of the loop's code block.
+  
+//   console.log(str[i]);: Inside the loop, this line uses the console.log() function to print the character at the current index i of the string str. 
+// str[i] accesses the character at the index i within the string.
+  
+//   }: This closing curly brace marks the end of the loop's code block.
